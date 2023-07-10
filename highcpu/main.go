f71ba50c341c8cb1a5b48c961bca33b5faf5cd0d
flowerinthenight <flowerinthenight@pm.me>
2023-07-10 14:21:31 +0900

    chore: highcpu flag

--- highcpu/main.go
+++ highcpu/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"flag"
 	"runtime"
 	"time"
 )
 
+var (
+	multi = flag.Bool("multi", false, "Set to true to use all CPU/cores")
+)
+
 func main() {
+	flag.Parse()
+	numCpu := 1
+	if *multi {
+		numCpu = runtime.NumCPU()
+	}
+
 	done := make(chan int)
-	for i := 0; i < runtime.NumCPU(); i++ {
-		// for i := 0; i < 2; i++ {
+	for i := 0; i < numCpu; i++ {
 		go func() {
 			for {
 				select {
