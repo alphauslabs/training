02f26a9e5523a47ff95374fb93e0f92fb7ccddfd
flowerinthenight <flowerinthenight@pm.me>
2023-07-10 12:17:08 +0900

    chore: mtx

--- racecondition/main.go
+++ racecondition/main.go
@@ -7,16 +7,19 @@ import (
 )
 
 func main() {
+	// var mtx sync.Mutex
 	counter := 0
 	const num = 5000
 	var wg sync.WaitGroup
 	wg.Add(num)
 	for i := 0; i < num; i++ {
 		go func() {
+			// mtx.Lock()
 			temp := counter
 			runtime.Gosched()
 			temp++
 			counter = temp
+			// mtx.Unlock()
 			wg.Done()
 		}()
 	}
